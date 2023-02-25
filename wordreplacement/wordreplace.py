0347d81d48fd5f664fcfbd01954d43eb5981c39a
Satya <satya.murthy@gmail.com>
2023-02-25 12:14:03 +0530

    word replacement function

---
+++ wordreplacement/wordreplace.py
@@ -0,0 +1,8 @@
+def replace_word():
+    
+    str = "hi guys, i am callign from hello hello hello"
+    word_to_replace = input("enter the word to replace")
+    word_replacement = input("enter the new word")
+    print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
